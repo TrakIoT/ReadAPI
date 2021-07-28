e7dd9c7b9c4bafec8cf2d8c02eb6e03d416e0965
datovard <abadtovar@gmail.com>
2021-07-27 23:44:25 -0500

    Cleaning files

--- conf/db/initdb.d/create-app-user.sh
+++ conf/db/initdb.d/create-app-user.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# https://www.stuartellis.name/articles/shell-scripting/#enabling-better-error-handling-with-set
 set -Eeuo pipefail
-    
-# Based on mongo/docker-entrypoint.sh
-# https://github.com/docker-library/mongo/blob/master/docker-entrypoint.sh#L303
 
 mongosh -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "$rootAuthDatabase" "$MONGO_INITDB_DATABASE" < /docker-entrypoint-initdb.d/charge-db.js
-
-# if [ "$MONGO_INITDB_USERNAME" ] && [ "$MONGO_INITDB_PASSWORD" ]; then
-#     "${mongo[@]}" -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "$rootAuthDatabase" "$MONGO_INITDB_DATABASE" <<- charge-db.js
--- conf/docker/mongo.dockerfile
+++
@@ -1,3 +0,0 @@
-FROM mongo:3.6
-
-COPY ./conf/db/db.js /docker-entrypoint-initdb.d
