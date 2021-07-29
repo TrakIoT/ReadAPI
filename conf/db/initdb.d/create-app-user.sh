bf61a5b730074adc9d641cbcf601b23ba3b01ed7
datovard <abadtovar@gmail.com>
2021-07-29 01:31:57 -0500

    Adding changes

--- conf/db/initdb.d/create-app-user.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -Eeuo pipefail
-
-mongosh -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "$rootAuthDatabase" "$MONGO_INITDB_DATABASE" < /docker-entrypoint-initdb.d/charge-db.js
