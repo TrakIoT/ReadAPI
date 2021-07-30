95012bdab6fdc2584fc7ab864a560cfae2358b7c
datovard <abadtovar@gmail.com>
2021-07-30 00:51:33 -0500

    Changes

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path));
-app.use('/sharks', sharks);
+app.use('/', sharks);
 
 app.listen(port, function () {
   console.log('Example app listening on ${port}!')
