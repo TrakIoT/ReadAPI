7d0a712cc4c2fec10faef98646e980ff8272f2be
datovard <abadtovar@gmail.com>
2021-08-02 01:13:20 -0500

    Fixes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 const db = require('./db');
-const bodyParser = require('body-parser');
-const sharks = require('./routes/sharks');
+const routes = require('./routes/readings.routes');
 
-const routes = require('./routes/routes');
-
-const path = __dirname + '/views/';
 const port = process.env.PORT || 8080;;
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/', sharks);
 
 app.use("/reading", routes);
+
+router.use(function(req, res, next) {
+  response = { 
+      message: 'URL not found'
+  };
+
+  res.status(404).send(response);
+});
 
 app.listen(port, function () {
   console.log('Example app listening on ${port}!')
--- src/routes/readings.routes.js
+++ src/routes/readings.routes.js
@@ -6,12 +6,4 @@ const router = express.Router();
 router.get('/limit/:limit/offset/:offset/filters/:product_id?/:server_id?', readingGetController);
 router.post('/', readingPostController);
 
-router.use(function(req, res, next) {
-  response = { 
-      message: 'URL not found'
-  };
-
-  res.status(404).send(response);
-});
-
 module.exports = router;
